798236cefa68e6fd98689dbd25cd229a73bb4c95
Tomek Leszczynski <hiszpania2018@gmail.com>
2023-05-04 15:13:40 +0200

    Button added. User may clear up the container and set new number of squares during work.

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const container = document.querySelector('.box-container')
+const button = document.querySelector('button')
 
-let grid = 64
-for (let i = 0; i < grid * grid; i++) {
-	const item = document.createElement('div')
-	item.classList.add('box')
-	item.style.height = `calc(100%/${grid})`
-	item.style.flexBasis = `calc(100%/${grid})`
-	document.body.append(item)
-	container.appendChild(item)
+function handlePrompt() {
+	let gridValue = parseInt(prompt('Please set the number of squares per side. Max value is 100', 16))
+	if (gridValue > 100) {
+		gridValue = 100
+	}
+	clearAll()
+	handleSquares(gridValue)
 }
-
+function handleSquares(value) {
+	for (let i = 0; i < value * value; i++) {
+		const item = document.createElement('div')
+		item.classList.add('box')
+		item.style.height = `calc(100%/${value})`
+		item.style.flexBasis = `calc(100%/${value})`
+		document.body.append(item)
+		container.appendChild(item)
+	}
+}
+function clearAll() {
+	const squares = container.querySelectorAll('.box')
+	squares.forEach(square => square.remove())
+}
 let mouseDown = false
 function changeColor(e) {
-	if (e.target.classList.contains('box') && mouseDown == true) {
+	if (e.target.classList.contains('box') && mouseDown) {
 		e.target.style.backgroundColor = 'red'
 	}
 }
-
+button.addEventListener('click', handlePrompt)
 container.addEventListener('mouseover', changeColor)
 container.addEventListener('mousedown', () => (mouseDown = true))
 container.addEventListener('mouseup', () => (mouseDown = false))
